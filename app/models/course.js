ee6485de56110700db6d02b9ffc066042f962fce
siddharth_dumbre <dumbre.si@northeastern.edu>
2023-11-19 21:50:53 -0500

    Removed debugged code

--- app/models/course.js
+++ app/models/course.js
@@ -6,47 +6,47 @@ const courseSchema = new schema({
   title: {
     type: String,
     required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  fees: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subCategory: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  noOfModules: {
+    type: Number,
+    required: true,
+  },
+  creationTime: {
+    type: String,
+    required: true,
+  },
+  avg_star_rating:{
+    type: Number,
+    required: false,
   }
-  // instructor: {
-  //   type: String,
-  //   required: true,
-  // },
-  // duration: {
-  //   type: String,
-  //   required: true,
-  // },
-  // fees: {
-  //   type: Number,
-  //   required: true,
-  // },
-  // category: {
-  //   type: String,
-  //   required: true,
-  // },
-  // subCategory: {
-  //   type: String,
-  //   required: true,
-  // },
-  // thumbnail: {
-  //   type: String,
-  //   required: true,
-  // },
-  // description: {
-  //   type: String,
-  //   required: true,
-  // },
-  // noOfModules: {
-  //   type: Number,
-  //   required: true,
-  // },
-  // creationTime: {
-  //   type: String,
-  //   required: true,
-  // },
-  // avg_star_rating:{
-  //   type: Number,
-  //   required: false,
-  // }
 },
 {
   versionKey: false
