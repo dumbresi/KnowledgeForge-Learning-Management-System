212b210ede40d112f91b841907f7049f0d45c5b2
siddharth_dumbre <dumbre.si@northeastern.edu>
2023-11-19 19:45:46 -0500

    Changed file paths

---
+++ app/models/course.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  fees: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subCategory: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  noOfModules: {
+    type: Number,
+    required: true,
+  },
+  creationTime: {
+    type: Date,
+    default: Date.now,
+  },
+  avg_star_rating:{
+    type: Number,
+    required: false,
+  }
+},
+{
+  versionKey: false
+}
+);
+
+const Course = Schema.model('Course', courseSchema);
+
+module.exports = Course;
---
+++ app/services/course-service.js
@@ -0,0 +1,20 @@
+import Course from '../models/course.js'
+
+export const searchCourse = async (params={}) => {
+    const courses=await  Course.find(params).exec();
+    return courses;
+}
+
+export const saveCourse = async (newCourse)=>{
+    const course = new Course(newCourse);
+    return await course.save();
+}
+
+export const updateCourse = async(updatedCourse,id)=>{
+    const course = await Course.findByIdAndUpdate(id,updatedCourse).exec();
+    return course;
+}
+
+export const removeCourse = async(id)=>{
+    return await Course.findByIdAndRemove(id).exec();
+}
