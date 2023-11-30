8111f3992b0d68e381c4c280ee92c5c531a202e8
Gaurav Popat Gunjal <gunjal.ga@northeastern.edu>
2023-11-30 14:49:27 -0500

    added dotenv and removed credentials

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import initialize from "./app/app.js";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const port =3000;
+const port =process.env.PORT;
 initialize(app);
 
 app.listen(port,()=>console.log("listening on port 3000"))
