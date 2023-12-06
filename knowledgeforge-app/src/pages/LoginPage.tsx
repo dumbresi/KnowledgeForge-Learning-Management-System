12d19f79fee57a1311f716a419fe811176f2defd
siddharth_dumbre <dumbre.si@northeastern.edu>
2023-12-06 00:22:06 -0500

    Login code modification

--- knowledgeforge-app/src/pages/LoginPage.tsx
+++ knowledgeforge-app/src/pages/LoginPage.tsx
@@ -1,46 +1,47 @@
-import { useState } from 'react'
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-function App() {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+function App(): JSX.Element {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-	async function loginUser(event:any) {
-		event.preventDefault()
+  async function loginUser(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
 
-		const response = await fetch('http://localhost:4000/auth/user/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		})
+    const response = await fetch('http://localhost:4000/auth/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
-		const data = await response.json()
-	}
+    const data = await response.json();
+    // Handle the 'data' as needed
+  }
 
-	return (
-		<div>
-			<h1>Login</h1>
-			<form onSubmit={loginUser}>
-				<input
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					type="email"
-				/>
-				<br />
-				<input
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					type="password"
-				/>
-				<br />
-				<input type="submit" value="Login" />
-			</form>
-		</div>
-	)
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginUser}>
+        <input
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          type="email"
+        />
+        <br />
+        <input
+          value={password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          type="password"
+        />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
