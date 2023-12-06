681561abe64ff43e45c81846fbfc1db03e2e57c5
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2023-12-05 23:30:57 -0500

    user-model

--- knowledgeforge-app/src/models/user.ts
+++ knowledgeforge-app/src/models/user.ts
@@ -1,19 +1,10 @@
-interface User {
-    user_id: string,
-    user_name: string,
-    contact_number: string,
-    email_id: string,
-    profile_picture: string
+interface User{
+    userID: string,
+    userName: string,
+    email: string,
+    profile_picture: string,
+    contactNumber: string,
+    access_token: string
 }
-
-export const users: User[] = [
-    {
-    user_id: '1',
-    user_name: 'John',
-    contact_number: '+1 123456789',
-    email_id: 'johnwick@gmail.com',
-    profile_picture: 'cool picture'
-    }
-];
 
 export default User;
