76870347d073263adbceed4161ee3a776b7c0b26
Gaurav Popat Gunjal <gunjal.ga@northeastern.edu>
2023-12-06 00:13:17 -0500

    added react router and tailwind setup

--- knowledgeforge-app/src/App.tsx
+++ knowledgeforge-app/src/App.tsx
@@ -2,15 +2,26 @@ import React, {useState} from 'react';
 import courses from './models/Course';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AllCoursePage from './pages/AllCoursePage';
 import RegisterUserPage from './pages/RegisterUserPage';
 import LoginPage from './pages/LoginPage';
+import InstructorDetails from './pages/InstructorDetails';
 
 function App() {
+  
   return (
-    <div className="coursepage">
-      <LoginPage/>
-    </div>
+    
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<AllCoursePage/>}/>
+				<Route path="/user/register" element={<RegisterUserPage/>} />
+				<Route path="/user/login" element={<LoginPage/>} />
+        {/* <Route path="/instructor/profile" element={<InstructorDetails(...instructor)/>} /> */}
+      </Routes>
+				
+			</BrowserRouter>
+    
   );
 }
 
