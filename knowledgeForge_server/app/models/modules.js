a40213324b75d9ca6ca43329aa3d546f1c84c9a0
Gaurav Popat Gunjal <gunjal.ga@northeastern.edu>
2023-12-09 02:52:27 -0500

    changed getModule api logic

--- knowledgeForge_server/app/models/modules.js
+++ knowledgeForge_server/app/models/modules.js
@@ -16,10 +16,7 @@ const moduleSchema = new schema({
         type: Number,
         required: true,
     },
-    no_of_lectures: {
-        type: Number,
-        required: true,
-    },  
+     
     description: {
         type: String,
         required: true,
@@ -29,11 +26,11 @@ const moduleSchema = new schema({
         type: Date,
         default: Date.now,
       },
-    video:{
+    videoId:{
       type: ObjectId,
       required: true,
     },
-    courseID:{
+    courseId:{
       type:String,
       required:true
     }   
