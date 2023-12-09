09844f2a2ba25551781d864c58bd4b8cc35a792f
siddharth_dumbre <dumbre.si@northeastern.edu>
2023-12-09 04:01:40 -0500

    Course Detail page implementation

--- knowledgeforge-app/src/components/CourseDetails.tsx
+++ knowledgeforge-app/src/components/CourseDetails.tsx
@@ -1,32 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import * as moduleService from '../services/module-services'
 import Course from '../models/Course';
+import ModuleCard from './ModuleCard';
+import Module from '../models/modules';
 
 type Props = {}
 
 const CourseDetails = (props: Props) => {
     const location = useLocation();
     const course: Course = location.state;
+    const [modules,setModules]= useState<Module[]>([]);
+    const [selectedModule,setSelectedModule]=useState<Module>();
 
-    const getModules=()=>{
-        const response=  moduleService.getModules(course._id);
-    }
-
-    getModules();
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await moduleService.getModules(course._id);
+          setModules(data);
+          setSelectedModule(data[0]); // Access the first element of the updated state
+        } catch (error) {
+          console.error('Error fetching course data:', error);
+        }
+      };
     
+      fetchData();
+    }, []);
+    
+    const changeSelectedModule=(mod:Module)=>{
+      setSelectedModule(mod);
+    }
+  
 
 
   return (
     <div>
         <div className='flex flex-row m-auto justify-between m-10'>
+          <div className='flex flex-col'>
             <h1>{course.title}</h1>
+            <p>{course.description}</p>
+          </div>
+            
             <img className='mr-20'alt='loading' src={`${course.thumbnail}`}></img>
         </div>
 
-        <div>
-          Modules
+        <div className='p-1 w-full  bg-black'></div>
+        {/* Divider line between course details and modules */}
+
+        {/* Module container */}
+        <div className='flex flex-row justify-between'>
+          <div>
+            <li>{`${selectedModule?.title}`}</li>
+            <li>{`${selectedModule?.description}`}</li>
+            <li>"Duration"+{`${selectedModule?.duration}`}</li>
+          </div>
+            <div className='mr-20'>
+              <div>
+                {modules.map(
+                        (moduleItem:Module) =>( 
+                          <div onClick={() => changeSelectedModule(moduleItem)}>
+                          <ModuleCard module={moduleItem} />
+                          </div>
+                        )
+                    )}
+              </div>
+          </div>
+
+
         </div>
+        
 
     </div>
   )
---
+++ knowledgeforge-app/src/components/ModuleCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Modules from '../models/modules'
+
+type ModuleCardProps = {
+    module: Modules;
+}
+
+const ModuleCard = (props: ModuleCardProps) => {
+    const { module } = props;
+
+    return (
+        <div>
+            <div>
+                <h1 className='bg-light_blue m-2'>{module.title}</h1>
+            </div>
+        </div>
+    );
+}
+
+
+export default ModuleCard
