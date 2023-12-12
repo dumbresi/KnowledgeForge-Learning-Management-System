3a8689cf700efca1233df6068c526df87b53cc6b
vaibvg <gohil.va@northeastern.edu>
2023-12-12 11:27:46 -0500

    Add course UI designed

--- knowledgeforge-app/src/pages/AddCoursePage.tsx
+++ knowledgeforge-app/src/pages/AddCoursePage.tsx
@@ -3,18 +3,19 @@ import AddCourseCard from "../components/AddCourseCard";
 import Sidebar from "../components/Sidebar";
 
 const AddCoursePage = () => {
-    const category=()=>{
-
-    }
-    return (
-        <div className="flex bg-background-cream">
-            <Sidebar category={category}/>
-            <div className='flex'>
-            <AddCourseCard/>
+  const category = () => {};
+  return (
+    <div className="flex flex-col">
+      <div className="flex bg-background-cream">
+        <Sidebar category={category} />
+        <div className="flex flex-col justify-between items-center p-4">
+          <div className="flex flex-col justify-between items-center bg-white rounded-lg p-8 shadow-2xl w-full md:w-96">
+            <AddCourseCard />
+          </div>
         </div>
-        </div>
-    );
-
+      </div>
+    </div>
+  );
 };
 
 export default AddCoursePage;
