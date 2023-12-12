8ff0cac67ced1ab6c0f714ca5198b40c434bb9e9
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2023-12-12 17:29:31 -0500

    add course ui

--- knowledgeforge-app/src/pages/AddCoursePage.tsx
+++ knowledgeforge-app/src/pages/AddCoursePage.tsx
@@ -5,11 +5,11 @@ import Sidebar from "../components/Sidebar";
 const AddCoursePage = () => {
   const category = () => {};
   return (
-    <div className="flex flex-row">
-      <div className="flex bg-background-cream">
+    <div className="flex flex-row bg-zinc-100">
+      <div className="flex bg-zinc-100">
         <Sidebar category={category} />
       </div>
-      <div className="flex w-1/2 h-3/5 my-5 justify-between items-center bg-white rounded-lg ml-32 p-8 shadow-2xl">
+      <div className="flex-grow flex justify-center items-center p-4">
         <AddCourseCard />
       </div>
     </div>
