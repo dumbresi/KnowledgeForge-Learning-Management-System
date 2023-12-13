8ebbd083d8ae5d84bf707c888a7a55a05ea2ecde
vaibvg <gohil.va@northeastern.edu>
2023-12-13 17:19:04 -0500

    Account Type Details Added

--- knowledgeforge-app/src/pages/UserCourses.tsx
+++ knowledgeforge-app/src/pages/UserCourses.tsx
@@ -1,41 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import * as UserSerivce from '../services/user-service';
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux/store';
-import Course from '../models/Course';
-import CoursesGrid from '../components/CoursesGrid';
-import Sidebar from '../components/Sidebar';
+import React, { useEffect, useState } from "react";
+import * as UserSerivce from "../services/user-service";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import Course from "../models/Course";
+import CoursesGrid from "../components/CoursesGrid";
+import Sidebar from "../components/Sidebar";
 
-type Props = {}
+type Props = {};
 // Show user's enrolled courses in the page
-const UserCourses  = (props: Props) => {
-  const [enrolledCourses,setEnrolledCourses]=useState<Course[]>([])
-  const { currentUser, loading, error } = useSelector((state:RootState)=>state.user);
+const UserCourses = (props: Props) => {
+  const [enrolledCourses, setEnrolledCourses] = useState<Course[]>([]);
+  const { currentUser, loading, error } = useSelector(
+    (state: RootState) => state.user
+  );
 
-  useEffect(()=>{
-    if(currentUser){
-      const fetchRegisteredCourses=async ()=>{
+  useEffect(() => {
+    if (currentUser) {
+      const fetchRegisteredCourses = async () => {
         const response = await UserSerivce.getRegisteredCourses();
         setEnrolledCourses(response);
-      }
+      };
       fetchRegisteredCourses();
     }
-    
-  },[])
+  }, []);
   return (
-    <div className=' h-full'>
-      <div className='flex'>
-        <div className=' flex'>
-          <Sidebar category={()=>{}}/>
+    <div className="">
+      <div className="flex">
+        <div className="flex h-screen">
+          <Sidebar category={() => {}} />
         </div>
-        <div className="w-[95%] h-full rounded-md bg-background_cream ab ">
-        {
-        (currentUser && enrolledCourses!==undefined)?<CoursesGrid courses={enrolledCourses}/>: "Please Login to see enrolled courses"
-        }
+        <div className="w-[95%] h-screen rounded-md bg-background_cream ab ">
+          {currentUser && enrolledCourses !== undefined ? (
+            <CoursesGrid courses={enrolledCourses} />
+          ) : (
+            "Please Login to see enrolled courses"
+          )}
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default UserCourses;
